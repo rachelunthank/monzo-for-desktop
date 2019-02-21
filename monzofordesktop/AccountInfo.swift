de73ee5b9ae6ef45c433bd8db945064e2a25685c
Rachel Unthank <rachelunthank@gmail.com>
2019-02-21 20:59:59 +0000

    Add scheme, remove token details

--- monzofordesktop/AccountInfo.swift
+++ monzofordesktop/AccountInfo.swift
@@ -10,9 +10,9 @@ import Foundation
 
 class AccountInfo {
 
-    public static let accessToken = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJlYiI6Im9BYVdXMTZJTytBN2ovOVNIRmxMIiwianRpIjoiYWNjdG9rXzAwMDA5ZjhicGxTYUd5YTg2SDlZaDciLCJ0eXAiOiJhdCIsInYiOiI1In0.fnTTg8fUmNyEouR-dJm00TmEbu9CUQZ5eDHA00Hd15fBUSj9O-5uMcOQ7QKGOXHCIWYL505Wpr5JKK0WzizW8A"
+    public static let accessToken = ""
 
-    public static var accountId = "acc_00009QaQi8HDCkEhybebL7"
+    public static var accountId = ""
 
     public static func updateAccountId(with id: String) {
         accountId = id
